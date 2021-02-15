a1797dd440d0a0950c17bc22a98a0c976926fd43
RolandOver9000 <csosz.roland2@gmail.com>
2021-02-15 19:32:34 +0100

    implemented bank code process

--- bank_ocr/code_reader.py
+++ bank_ocr/code_reader.py
@@ -1,6 +1,75 @@
 
 DUMMY_FILE_NAME = "data/dummy_data.txt"
 NUMBER_OF_CHARACTERS_IN_LINE = 27
+NUMBER_OF_DIGIT_PRINT_LINE = 3
+NUMBER_OF_DIGITS = 9
+DIGIT_CHARACTER_COLUMN = 3
+DICT_OF_STRING_DIGITS = {
+    " _ "
+    "| |"
+    "|_|": 0,
+    "   "
+    "  |"
+    "  |": 1,
+    " _ "
+    " _|"
+    "|_ ": 2,
+    " _ "
+    " _|"
+    " _|": 3,
+    "   "
+    "|_|"
+    "  |": 4,
+    " _ "
+    "|_ "
+    " _|": 5,
+    " _ "
+    "|_ "
+    "|_|": 6,
+    " _ "
+    "  |"
+    "  |": 7,
+    " _ "
+    "|_|"
+    "|_|": 8,
+    " _ "
+    "|_|"
+    " _|": 9
+}
+
+
+def process_string_code(code):
+    """
+    Processes the bank code. It handles the string code like a matrix. Each code is a 3x3 string. For example
+    The inner for goes from 0:0 -> 0:3 then steps 1 line and concatenate 1:0 -> 1:3 then steps 1 line
+    and concatenate 2:0 -> 2:3. Then the outer for "step a digit" and the inner for goes again.
+    Then it searches the string digit in the dictionary of digits and concatenate the numbers.
+    :param code: String of a bank code that broken into lines and concatenated.
+    :return:
+    """
+    processed_code = ""
+
+    for index in range(NUMBER_OF_DIGITS):
+        digit = ""
+        starter_column_of_digit = index * DIGIT_CHARACTER_COLUMN
+        for row in range(NUMBER_OF_DIGIT_PRINT_LINE):
+            row_starter_index = starter_column_of_digit + (row * NUMBER_OF_CHARACTERS_IN_LINE)
+            digit += code[row_starter_index: row_starter_index + DIGIT_CHARACTER_COLUMN]
+        processed_code += str(DICT_OF_STRING_DIGITS[digit])
+    return processed_code
+
+
+def process_read_lines(read_codes):
+    """
+    Processes the rows of the file.
+    :param read_codes: List of strings
+    Returns:
+        List of the read codes.
+    """
+    code_list = []
+    for code in read_codes:
+        code_list.append(process_string_code(code))
+    print(code_list)
 
 
 def read_from_dummy_file():
@@ -33,3 +102,4 @@ def handle_process():
     Handles the process of code reader.
     """
     read_lines = read_from_dummy_file()
+    process_read_lines(read_lines)
