c59c720e2def9eeca0fd89db8835c4a455661288
RolandOver9000 <csosz.roland2@gmail.com>
2021-02-17 17:52:20 +0100

    implemented some test for try_to_fix_digit function

--- test/test_code_fixer.py
+++ test/test_code_fixer.py
@@ -29,6 +29,38 @@ class TestSum(unittest.TestCase):
         result = bank_ocr.is_digit_valid(test_code)
         self.assertEqual(result, False, "The digit should be invalid.")
 
+    # -----------try_to_fix function tests----------
+
+    def test_try_to_fix_digit_with_fixable_decimal_digit(self):
+        """
+        Tests code_fixer's try_to_fix_digit function with fixable decimal digit.
+        """
+        test_digit = "   " \
+                     "   " \
+                     "  |"
+        result = bank_ocr.try_to_fix_digit(test_digit)
+        self.assertEqual(result, ['1'], "The result should be '1' in a list.")
+
+    def test_try_to_fix_digit_with_fixable_hexadecimal_digit(self):
+        """
+        Tests code_fixer's try_to_fix_digit function with fixable hexadecimal digit.
+        """
+        test_digit = " _ " \
+                     "|  " \
+                     " _ "
+        result = bank_ocr.try_to_fix_digit(test_digit)
+        self.assertEqual(result, ['C'], "The result should be 'C' in a list.")
+
+    def test_try_to_fix_digit_with_completely_wrong_digit(self):
+        """
+        Tests code_fixer's try_to_fix_digit function with fixable decimal digit.
+        """
+        test_digit = "   " \
+                     "  |" \
+                     "|_|"
+        result = bank_ocr.try_to_fix_digit(test_digit)
+        self.assertEqual(result, [], "The result should be an empty list.")
+
 
 if __name__ == '__main__':
     unittest.main()
