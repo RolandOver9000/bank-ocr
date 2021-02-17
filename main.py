e4a02b1504a671c4fbfd29e0746c95d7d0afbaf3
RolandOver9000 <csosz.roland2@gmail.com>
2021-02-17 14:45:42 +0100

    removed menu

--- main.py
+++ main.py
@@ -1,40 +1,8 @@
-import util
-import sys
-
-from bank_ocr import sub_menu_handler
-
-
-def choose():
-    user_input = util.get_input()
-    if user_input == "1":
-        sub_menu_handler.handle_user_story_1_sub_menu()
-    elif user_input == "2":
-        sub_menu_handler.handle_user_story_2_sub_menu()
-    elif user_input == "3":
-        sub_menu_handler.handle_user_story_3_sub_menu()
-    elif user_input == "4":
-        sub_menu_handler.handle_user_story_4_sub_menu()
-    elif user_input == "0":
-        sys.exit(0)
-    else:
-        raise KeyError("There is no such option.")
-
-
-def handle_menu():
-    options = ["Analyze data from dummy file (User story 1)",
-               "Validate data from dummy file (User story 2)",
-               "Handle wrong code (User story 3)",
-               "Try to fix wrong code and hexadecimal numbers (User story 4, User story 5)"]
-
-    util.print_menu("Main menu", options, "Exit program")
+from bank_ocr import code_fixer
 
 
 def main():
-    handle_menu()
-    try:
-        choose()
-    except KeyError as err:
-        util.print_error_message(str(err))
+    print(code_fixer.handle_code_fix())
 
 
 if __name__ == '__main__':
