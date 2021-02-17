bdcf5545100093b25f7f1c05fc532d595bcc0df2
RolandOver9000 <csosz.roland2@gmail.com>
2021-02-17 20:46:29 +0100

    implemented a test for get_calculated_checksum function

--- test/test_code_validator.py
+++ test/test_code_validator.py
@@ -9,10 +9,11 @@ class TestSum(unittest.TestCase):
 
     def test_get_calculated_checksum_with_valid_code(self):
         """
-        Tests code_validator's get_calculated_checksum function with valid code.
+        Tests code_validator's get_calculated_checksum function with valid decimal code.
         """
-        test_code = 711111111
-        result = bank_ocr.get_calculated_checksum()
+        test_code = list("711111111")
+        result = bank_ocr.get_calculated_checksum(test_code)
+        self.assertEqual(result, 99)
 
 
 if __name__ == '__main__':
